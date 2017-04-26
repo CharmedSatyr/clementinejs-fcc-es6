56d4d1ad69f9bb98a729a41760f392947a4cc17b
CharmedSatyr <z@charmed.tech>
2017-04-26 22:09:21 +0900

    Fully functional

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # clementinejs-fcc-es6
-Clementine.js boilerplate for freeCodeCamp (includes GitHub authentication using Passport.js). Updated to use ES6 syntax.
+Clementine.js boilerplate for freeCodeCamp (includes GitHub authentication using Passport.js). Updated to use ES6 syntax. Original: https://github.com/johnstonbl01/clementinejs-fcc
---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express'),
+   routes = require('./app/routes/index.js'),
+   mongoose = require('mongoose'),
+   passport = require('passport'),
+   session = require('express-session');
+
+const app = express();
+require('dotenv')
+   .load();
+require('./app/config/passport')(passport);
+
+const port = process.env.PORT || 8080,
+   mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/clementinejs-fcc-es6';
+
+const path = process.cwd();
+
+mongoose.connect(mongo_uri);
+mongoose.Promise = global.Promise;
+
+app.use('/controllers', express.static(path + '/app/controllers'));
+app.use('/views', express.static(path + '/views'));
+app.use('/common', express.static(path + '/app/common'));
+
+app.use(session({
+   secret: 'secretCharm',
+   resave: false,
+   saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+
+app.listen(port, () => {
+   console.log('Node.js listening on port ' + port + '...');
+});
